bb4f1bc0b40f4ffb505f9e24ddc2e96a6bcb1283
westsidefoodbank <yvonne@wsfb.org>
2015-12-04 15:17:12 -0800

    test file

---
+++ dispatch/index1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Food Drive Scheduler</title>
+
+<link type="text/css" href="css/ui-lightness/jquery-ui-1.8.21.custom.css" rel="stylesheet" />
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.8.21.custom.min.js"></script>
+</head>
+
+<body>
+
+<p><a href="orgadd.php" target="scheduler">Organization</a> Food Drive Dropoff Pickup</p>
+
+<iframe name="scheduler" frameborder="0" style="width:100%; height:100%;">
+Sorry, your browser does not support this feature.
+</iframe>
+
+
+</body>
+</html>
