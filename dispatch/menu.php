6bf0a5b3ef3548f0bd572bf25c16a020f70efb64
westsidefoodbank <yvonne@wsfb.org>
2015-12-04 15:18:47 -0800

    scheduler menu

---
+++ dispatch/menu.php
@@ -0,0 +1,53 @@
+<link rel="stylesheet" href="menu.css" type="text/css">
+
+<ul id="menu">
+<li><a href="importform.php">Import</a></li>
+<li><a href="orgdisplay.php">Organizations</a>
+  <ul class="menu first">
+  <li><a href="orgdisplay.php">Organization List</a></li>
+  <li><a href="orgadd.php">Add an Organization</a></li>
+  <li><a href="orgcatdisplay.php">Organization Categories</a>
+    <ul class="menu">
+    <li><a href="orgcatdisplay.php">Category List</a></li>
+    <li><a href="orgcatadd.php">Add a new Category</a></li>
+    </ul></li>
+    </ul>
+</li>
+<li><a href="fooddrivedisplay.php">Food Drives</a>
+  <ul class="menu first">
+  <li><a href="fooddrivedisplay.php">Food Drive List</a></li>
+  <li><a href="fooddriveadd.php">Add a new Food Drive</a></li>
+  <li><a href="fooddrivecompleted.php">Completed Food Drives</a></li>  
+  </ul>
+</li>
+<li><a href="dropoffpickupdisplay.php">Dropoffs and Pickups</a>
+  <ul class="menu first">
+  <li><a href="dropoffdisplay.php">Dropoff List</a></li>
+  <li><a href="pickupdisplay.php">Pickup List</a></li>
+  <li><a href="dropoffpickupdisplay.php">Dropoffs and Pickups List</a></li>
+  <li><a href="orgselect.php">Add a Dropoff or Pickup</a></li>
+  </ul>
+</li>
+<li><a href="calendar.php">Calendar</a>
+  <ul class="menu first">
+  <li><a href="calendar.php">Full Calendar</a></li>  
+  <li><a href="calendar.php?dropoff=1">Dropoff Calendar</a></li>
+  <li><a href="calendar.php?pickup=1">Pickup Calendar</a></li>
+  </ul>
+</li>
+<li><a href="staffdisplay.php">Staff</a>
+  <ul class="menu first">
+  <li><a href="staffdisplay.php">Staff List</a></li>
+  <li><a href="staffadd.php">Add a new Staff Member</a></li>
+  </ul>
+</li>  
+<li><a href="itemdisplay.php">Items</a>
+  <ul class="menu first">
+  <li><a href="itemdisplay.php">Item List</a></li>
+  <li><a href="itemadd.php">Add a new Item</a></li>
+  </ul>
+</li>
+
+</ul>
+
+<br><br>
